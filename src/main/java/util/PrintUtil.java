801d297ba0c5dc71efff3d0121dd5adc34d07f18
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-09-21 16:50:30 +0800

    ADD：添加java控制台颜色输出

---
+++ src/main/java/util/PrintUtil.java
@@ -0,0 +1,67 @@
+package util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by hzwangjian1 on 2017/9/21.
+ */
+public class PrintUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PrintUtil.class);
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+
+    public static void info(String message, String... args){
+        String format = "";
+        for(String arg : args){
+            format += arg;
+        }
+        logger.info(format + message + ANSI_RESET);
+    }
+
+    public static void debug(String message, String... args){
+        String format = "";
+        for(String arg : args){
+            format += arg;
+        }
+        logger.debug(format + message + ANSI_RESET);
+    }
+
+    public static void error(String message, String... args){
+        String format = "";
+        for(String arg : args){
+            format += arg;
+        }
+        logger.error(format + message + ANSI_RESET);
+    }
+
+    public static void testPrintColot() {
+        logger.info(ANSI_GREEN_BACKGROUND + "This text has a green background but default text!" + ANSI_RESET);
+        logger.info(ANSI_RED + "This text has red text but a default background!" + ANSI_RESET);
+        logger.info(ANSI_GREEN_BACKGROUND + ANSI_RED + "This text has a green background and red text!" + ANSI_RESET);
+    }
+
+    public static void main(String[] args) {
+//        testPrintColot();
+        info("test info", ANSI_RED, ANSI_CYAN_BACKGROUND);
+    }
+}
