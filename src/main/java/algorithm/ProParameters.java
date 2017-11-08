c4ae12530618a92c8a89952d42375a5df25ef20b
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-11-08 15:51:45 +0800

    MOD：所有配置参数通过ProParameters.properties访问

--- src/main/java/algorithm/ProParameters.java
+++ src/main/java/algorithm/ProParameters.java
@@ -3,6 +3,9 @@ package algorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.PathUtil;
+import util.ProperityHandler;
+
+import java.util.Properties;
 
 /**
  * Created by hzwangjian1 on 2017/11/8.
@@ -10,7 +13,13 @@ import util.PathUtil;
 public class ProParameters {
     private static Logger logger = LoggerFactory.getLogger(ProParameters.class);
 
-    public static final String PropertiesFilePath = PathUtil.getCurrentDir() + "/parameters.properties";
+    public final static String PropertiesFilePath = PathUtil.getCurrentDir() + "/parameters.properties";
+    //所有配置参数通过ProParameters.properties访问
+    public static Properties properties = null;
+
+    static {
+        properties = ProperityHandler.loadPropertiesFromFile(PropertiesFilePath);
+    }
 
     public static void main(String[] args) {
         logger.info(PropertiesFilePath);
