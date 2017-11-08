e5aca9875ce7f4dd050101d3652734ba6dd07932
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-11-08 10:11:47 +0800

    MOD：update ProperityHandler

--- src/main/java/util/ProperityHandler.java
+++ src/main/java/util/ProperityHandler.java
@@ -27,6 +27,40 @@ public class ProperityHandler {
         return properties;
     }
 
+    /**
+     * 更新properties文件
+     * @param propertiesFilePath
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean updateProperties(String propertiesFilePath, String key, String value){
+        Properties properties = new Properties();
+        try {
+            properties.load(new BufferedReader(new FileReader(new File(propertiesFilePath))));
+        }catch (Exception e){
+            logger.error("", e);
+            return false;
+        }
+        properties.setProperty(key, value);
+        return saveProperties(properties, propertiesFilePath);
+    }
+
+    /**
+     * properties保存到文件
+     * @param properties
+     * @param savePath
+     * @return
+     */
+    public static boolean saveProperties(Properties properties, String savePath){
+        try {
+            properties.store(new BufferedWriter(new FileWriter(savePath)), null);
+        }catch (Exception e){
+            logger.error("", e);
+            return false;
+        }
+        return true;
+    }
 
     /**
      * 从resources文件读取properties
@@ -51,6 +85,8 @@ public class ProperityHandler {
 
         Properties propertiesFromResource = loadPropertiesFromResource("servers.properties");
         logger.info(propertiesFromResource.getProperty("hostID"));
+
+        updateProperties("E://temp/code/servers.properties", "database", "ooxx");
     }
 
 }
