0b1f0fec59701fd3619765ed5259f7f0108eee79
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-11-29 14:56:56 +0800

    MOD：test LSH

--- src/main/java/algorithm_test/search/TestKdTree.java
+++ src/main/java/algorithm_test/search/TestKdTree.java
@@ -1,6 +1,7 @@
 package algorithm_test.search;
 
 import smile.neighbor.KDTree;
+import smile.neighbor.LSH;
 
 /**
  * Created by hzwangjian1 on 2017/8/4.
@@ -13,6 +14,7 @@ public class TestKdTree {
         Integer [] data = new Integer[10000];
 
         KDTree<Integer>  kdTree = new KDTree<Integer>(key, data);
+        LSH<Integer> lsh = new LSH<Integer>(key, data);
     }
 
     public static void main(String[] args) {
