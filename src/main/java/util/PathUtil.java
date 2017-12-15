19490ff0550db8f6fa8fff34b713a17e20b35713
hzwangjian1 <hzwangjian1@corp.netease.com>
2017-12-15 13:47:36 +0800

    ADD：添加历史特征目录删除接口

--- src/main/java/util/PathUtil.java
+++ src/main/java/util/PathUtil.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hzwangjian1 on 2017/8/18.
@@ -13,7 +16,7 @@ import java.io.File;
 public class PathUtil {
 
     private static Logger logger = LoggerFactory.getLogger(PathUtil.class);
-
+    private static SimpleDateFormat simpleDayFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     /**
      * 返回当前目录
@@ -54,6 +57,37 @@ public class PathUtil {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 删除dir下days之前的目录
+     * @param dir
+     *         dir下的子目录格式：2017-12-13  2017-12-14  2017-12-15  2017-12-16
+     * @param days
+     */
+    public static void cleanHistoryDirs(String dir, int days) {
+        String filtTime = simpleDayFormat.format(DateProcess.kDaysAgeDate(days));
+
+        try {
+            File file = new File(dir);
+            File[] subFiles = file.listFiles();
+            List<File> filesToDelete = new ArrayList<>();
+            for (File subFile : subFiles) {
+                if (subFile.isDirectory() && subFile.getName().compareTo(filtTime) < 0) {
+                    filesToDelete.add(subFile);
+                }
+            }
+
+            for (File subfile : filesToDelete) {
+                try {
+                    FileUtils.deleteDirectory(subfile);
+                } catch (Exception e) {
+                    logger.error("", e);
+                }
+            }
+        }catch (Exception e){
+            logger.error("", e);
+        }
     }
 
 }
